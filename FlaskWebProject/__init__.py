e25b5bc6f4287c34d4e723b4a105fd56f359feb8
praneshvv1985 <pvadrevu@gitam.in>
2026-03-10 13:47:50 +0530

    Implement logging configuration in __init__.py
    Added logging setup for production environment.

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,21 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
+# Logging setup
+if not app.debug:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s'
+    )
+    stream_handler.setFormatter(formatter)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.addHandler(stream_handler)
+    app.logger.info('FlaskWebProject startup')
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
