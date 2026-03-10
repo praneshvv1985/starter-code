b16efc987f607fdcc6c37f9fc8ca3657d025adff
praneshvv1985 <pvadrevu@gitam.in>
2026-03-10 15:10:32 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,6 +1,7 @@
 """
 The flask application package.
 """
+
 import logging
 from flask import Flask
 from config import Config
@@ -11,16 +12,11 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Logging setup
 if not app.debug:
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
-
-    formatter = logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s'
-    )
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
     stream_handler.setFormatter(formatter)
-
     app.logger.setLevel(logging.INFO)
     app.logger.addHandler(stream_handler)
     app.logger.info('FlaskWebProject startup')
